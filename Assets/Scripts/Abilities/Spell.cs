519998ed410acb3e8a9550e9b1064650baded906
feazeyu <hernifea@seznam.cz>
2025-08-14 17:48:46 +0200

    Formatting

--- Assets/Scripts/Abilities/Spell.cs
+++ Assets/Scripts/Abilities/Spell.cs
@@ -13,9 +13,12 @@ namespace Game.Abilities
         public List<SpellComponent> Components;
         [SerializeField, HideInInspector]
         private int _manaCost;
-        public int ManaCost {
-            get {
-                if (_manaCost == 0) {
+        public int ManaCost
+        {
+            get
+            {
+                if (_manaCost == 0)
+                {
                     _manaCost = Components.Sum(c => (int)c.resourceCosts[ResourceTypes.Mana]);
                 }
                 return _manaCost;
@@ -23,9 +26,12 @@ namespace Game.Abilities
         }
         [SerializeField, HideInInspector]
         private int _healthCost;
-        public int HealthCost {
-            get {
-                if (_healthCost == 0) {
+        public int HealthCost
+        {
+            get
+            {
+                if (_healthCost == 0)
+                {
                     _healthCost = Components.Sum(c => (int)c.resourceCosts[ResourceTypes.Health]);
                 }
                 return _healthCost;
@@ -33,49 +39,77 @@ namespace Game.Abilities
         }
         [SerializeField, HideInInspector]
         private int _staminaCost;
-        public int StaminaCost {
-            get {
-                if (_staminaCost == 0) {
+        public int StaminaCost
+        {
+            get
+            {
+                if (_staminaCost == 0)
+                {
                     _staminaCost = Components.Sum(c => (int)c.resourceCosts[ResourceTypes.Stamina]);
                 }
                 return _staminaCost;
             }
         }
         private float _cooldown;
-        public float Cooldown { 
-            get {
-                if (_cooldown == 0) {
+        public float Cooldown
+        {
+            get
+            {
+                if (_cooldown == 0)
+                {
                     _cooldown = Components.Sum(c => c.cooldown);
                 }
                 return _cooldown;
             }
         }
         private float _range;
-        public float Range { 
-            get {
-                if (_range == 0) {
+        public float Range
+        {
+            get
+            {
+                if (_range == 0)
+                {
                     _range = Components.Max(c => c.range);
                 }
                 return _range;
             }
         }
         private float _speed;
-        public float Speed { 
-            get {
-                if (_speed == 0) {
+        public float Speed
+        {
+            get
+            {
+                if (_speed == 0)
+                {
                     _speed = Components.Max(c => c.speed);
                 }
                 return _speed;
             }
         }
         private float _damage;
-        public float Damage { 
-            get {
-                if (_damage == 0) {
+        public float Damage
+        {
+            get
+            {
+                if (_damage == 0)
+                {
                     _damage = Components.Sum(c => c.damage);
                 }
                 return _damage;
             }
         }
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Assets/Create/Abilities/Spell")]
+        public static void CreateSpell()
+        {
+            var asset = ScriptableObject.CreateInstance<Spell>();
+            string path = UnityEditor.AssetDatabase.GenerateUniqueAssetPath("Assets/NewSpell.asset");
+            UnityEditor.AssetDatabase.CreateAsset(asset, path);
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.EditorUtility.FocusProjectWindow();
+            UnityEditor.Selection.activeObject = asset;
+        }
+#endif
     }
 }
--- Assets/Scripts/Abilities/SpellComponent.cs
+++ Assets/Scripts/Abilities/SpellComponent.cs
@@ -1,13 +1,29 @@
 using Game.Core.Utilities;
 using UnityEngine;
 using Game.Character;
-namespace Game.Abilities { 
-internal class SpellComponent : ScriptableObject
+using UnityEditor;
+namespace Game.Abilities
 {
-    public float speed;
-    public float damage;
-    public SerializableDictionary<ResourceTypes, float> resourceCosts;
-    public float cooldown;
-    public float range;
-}
+    internal class SpellComponent : ScriptableObject
+    {
+        public float speed;
+        public float damage;
+        public SerializableDictionary<ResourceTypes, float> resourceCosts;
+        public float cooldown;
+        public float range;
+        //Todo add optional aoe
+
+#if UNITY_EDITOR
+        [MenuItem("Assets/Create/Abilities/Spell Component")]
+        public static void CreateSpellComponent()
+        {
+            var asset = CreateInstance<SpellComponent>();
+            string path = AssetDatabase.GenerateUniqueAssetPath("Assets/SpellComponent.asset");
+            AssetDatabase.CreateAsset(asset, path);
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+#endif
+    }
 }
